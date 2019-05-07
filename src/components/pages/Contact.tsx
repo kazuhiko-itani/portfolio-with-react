b9b6a773eea41530153a0f6f67f5297bcaeb97d6
kazuhiko.itani <kazuhiko.itani@gmail.com>
2019-05-07 18:32:53 +0900

    netlify用の設定ファイルを作成

---
+++ src/components/pages/Contact.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from 'react';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import withStyles, { WithStyles, StyleRules } from '@material-ui/core/styles/withStyles';
+import createStyles from '@material-ui/core/styles/createStyles';
+import { Grid } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import TextField from '@material-ui/core/TextField';
+
+const styles = (theme: Theme): StyleRules => createStyles({
+    heading: {
+        padding: '10px 0'
+    },
+    form: {
+        width: '100%',
+        marginTop: 20
+    },
+    input: {
+        marginTop: 10
+    }
+});
+
+const Contact: FC<WithStyles<typeof styles>> = ({ classes }) => (
+    <div>
+        <div>
+            <Typography variant="h2" className={classes.heading}>お問い合わせ</Typography>
+            <p>
+                ご連絡はTwitter(
+                <a href="https://twitter.com/akashi__akashi" target="_blank" rel="noopener noreferrer">@akashi__akashi</a>
+                )のDM、または以下のフォームからお願いいたします。
+            </p>
+        </div>
+        <div>
+            <FormControl className={classes.form}>
+                <div>
+                    <FormLabel required={true}>お名前</FormLabel>
+                    <TextField placeholder="your name" fullWidth={true} className={classes.input}></TextField>
+                </div>
+            </FormControl>
+        </div>
+    </div>
+);
+
+export default withStyles(styles)(Contact);
